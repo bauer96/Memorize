b083fc5654ca7abf242fa3443e50d5387a51feb4
Hannes Bauer <zocken210@web.de>
2025-01-07 20:35:36 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hannes Bauer on 07.01.25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
